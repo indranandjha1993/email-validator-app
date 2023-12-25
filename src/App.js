316165df4da84f50725c98b40268c8a6efb0eb2c
Indra Nand Jha <indranandjha0@gmail.com>
2023-12-25 23:18:52 +0530

    inital app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [email, setEmail] = useState('');
+    const [responseMessage, setResponseMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false); // New state for loading status
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setIsLoading(true); // Start loading
+        try {
+            const response = await axios.post('http://localhost:5001/validate_email', {email});
+            setResponseMessage(`${response.data.reason}`);
+            setIsLoading(false); // Stop loading
+        } catch (error) {
+            console.error('Error:', error);
+            setResponseMessage('Validation failed');
+            setIsLoading(false); // Stop loading
+        }
+    };
+
+    return (
+        <div className="container">
+            <h1>Email Validator</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter email"
+                />
+                <button type="submit" disabled={isLoading}>Validate</button>
+            </form>
+            {isLoading ? <div>Loading...</div> : <div className="response">{responseMessage}</div>}
+        </div>
+    );
+
 }
 
 export default App;
